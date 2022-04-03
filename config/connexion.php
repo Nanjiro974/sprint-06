ae6400cc8b3e8b28c217528c758ce7f9f95e77b8
Nanjiro <armougom.t@gmail.com>
2022-04-03 23:18:13 +0400

    ===

--- config/connexion.php
+++ config/connexion.php
@@ -1,14 +1,20 @@
 <?php
 
 try {
-	if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
-	 $access=new pdo('mysql:host=109.234.164.161;dbname=sc1lgvu9627_armougom-thomas.sprint-06;charset=utf8', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
-     
-     $access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-
-} catch (Exception $e) 
-{
-	$e->getMessage();
+    // dev configuration
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
+        $bdd = new mysqli(
+            'localhost',
+            'root',
+            '',
+            'sprint06'
+        );
+    }
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+        $bdd = new mysqli('109.234.164.161', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], 'sc1lgvu9627_armougom-thomas.sprint-06');
+    }
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
 }
     
      
