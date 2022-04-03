13bf943ea2fcc3c4d13d29089eb6703f2862d23b
Nanjiro <armougom.t@gmail.com>
2022-04-03 22:29:04 +0400

    ^^

--- Vap Factory/config/connexion.php
+++ Vap Factory/config/connexion.php
@@ -1,7 +1,8 @@
 <?php
 
 try {
-	 $access=new pdo("mysql:host=localhost;dbname=sprint06;charset=utf8", "root", "");
+	if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+	 $access=new pdo('mysql:host=109.234.164.161;dbname=sc1lgvu9627_armougom-thomas.sprint-06;charset=utf8', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD']);
      
      $access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
