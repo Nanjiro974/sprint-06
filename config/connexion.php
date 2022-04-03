7e866843e2f02cac82f8ae707b329d29fe8c6e60
Nanjiro <armougom.t@gmail.com>
2022-04-04 00:05:57 +0400

    aaah

--- config/connexion.php
+++ config/connexion.php
@@ -1,21 +1,31 @@
 <?php
 
 try {
-    // dev configuration
-    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
-        $bdd = new mysqli(
-            'localhost',
-            'root',
-            '',
-            'sprint06'
-        );
-    }
-    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
-        $bdd = new mysqli('109.234.164.161', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], 'sc1lgvu9627_armougom-thomas.sprint-06');
-    }
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
+	$access=new mysqli('109.234.164.161', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], 'sc1lgvu9627_armougom-thomas.sprint-06');
+	
+	$access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+} catch (Exception $e) 
+{
+   $e->getMessage();
 }
+
+// try {
+//     // dev configuration
+//     if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
+//         $bdd = new mysqli(
+//             'localhost',
+//             'root',
+//             '',
+//             'sprint06'
+//         );
+//     }
+//     if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
+//         $bdd = new mysqli('109.234.164.161', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], 'sc1lgvu9627_armougom-thomas.sprint-06');
+//     }
+// } catch (Exception $e) {
+//     die('Erreur : ' . $e->getMessage());
+// }
     
      
 
