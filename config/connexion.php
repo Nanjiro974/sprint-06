791a6c7afa501d4f4a8aca75526d3fdb6f7f1228
Nanjiro <armougom.t@gmail.com>
2022-04-05 13:16:20 +0400

    ca marche

--- config/connexion.php
+++ config/connexion.php
@@ -1,8 +1,21 @@
 <?php
 
-try {
-	$access=new mysqli('109.234.164.161', $_SERVER['DB_USER'], $_SERVER['DB_PASSWORD'], 'sc1lgvu9627_armougom-thomas.sprint-06');
+// try {
+// 	$access=new pdo("mysql:host=localhost;dbname=vapfactory;charset=utf8", "root", "");
 	
+// 	$access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+// } catch (Exception $e) 
+// {
+//    $e->getMessage();
+// }
+
+try {
+	$access=new PDO(
+		'mysql:host=109.234.164.161;dbname=sc1lgvu9627_armougom-thomas.sprint-06;charset=utf8',
+		$_SERVER['DB_USER'],
+		$_SERVER['DB_PASSWORD']
+	  );
 	$access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
 } catch (Exception $e) 
